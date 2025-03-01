3b8b7dfc9091f2c4fcab94e8c8486136d33d07aa
Amir Hossein Shahidi <amirahsh20041382@gmail.com>
2025-03-01 17:06:10 +0330

    testing baghalixd11

--- src/main/kotlin/user/application/UserController.kt
+++ src/main/kotlin/user/application/UserController.kt
@@ -109,6 +109,7 @@ class UserController {
 
 
 
+
     fun changeInformation(params: ChangeInformationParams): UserResult<Unit, ResultFailure> {
         val usernameResult: UserResult<Username, Username.Failure> = Username.makeNew(params.username)
         val fullNameResult = FullName.makeNew(params.fullName)
